a52ad8858e20ed61dae99fcf77c152db8080bf21
Desislava Milusheva <deisi_deisi@abv.bg>
2020-10-22 18:08:14 +0300

    Setup database properties

--- src/main/java/com/dmilusheva/bikecompany/controllers/BikesController.java
+++ src/main/java/com/dmilusheva/bikecompany/controllers/BikesController.java
@@ -8,36 +8,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-// creat an API endpoint to pass the bike information from /models/Bike.java
-// "/api/v1/bikes"  is the path or the url that the application will respond to when a request is made
-// v1 is for version 1
-
-
+/**
+ * Creat an API endpoint to pass the bike information from /models/Bike.java
+ * "/api/v1/bikes"  is the path or the url that the application will respond to when a request is made.
+ *
+ *  v1 is for version 1
+ *
+ */
 @RestController
 @RequestMapping("/api/v1/bikes")
 public class  BikesController {
 
-    // @GetMapping  -> method level annotation , which is going to say
-    // when there is a GET request with the base path "/api/vi/bikes",
-    // this GET request will be forward to the list() method
-    // which will return a response with list of bikes
-    // list bikes
+
+    /**
+     * @GetMapping  -> method level annotation , which is going to say
+     * when there is a GET request with the base path "/api/vi/bikes",
+     * this GET request will be forward to the list() method
+     * which will return a response with list of bikes.
+     *
+     * @return list of bikes
+     */
     @GetMapping
     public List<Bike> list(){
         List<Bike> bikes = new ArrayList<>();
         return bikes;
     }
 
-    // This create() method will handle the Form creation when the user submits their registration information.
-    // The registration information will be processed by this method and then --> get() method.
-    // create bike
+
+    /**
+     * This create() method will handle the Form creation when the user submits their registration information.
+     * The registration information will be processed by this method and then --> get() method.
+     *
+     * @param bike - this method create new bike
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike){
     }
 
-    // The get() method, which will get a particular bike and return that single bike based off of the id that we pass it.
-    // get bike by id
+
+    /**
+     * The get() method, which will get a particular bike and return that single bike based off of the id that we pass it.
+     *
+     * What is doing?  ->  get bike by id
+     *
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id) {
         return new Bike();
